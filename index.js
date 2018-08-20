f00ce3172c1a5e9938363e5f2709d532f43fb01d
brandiw <brandimichelle.0911@gmail.com>
2018-08-20 15:44:37 -0700

    Added home page and controllers

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Require stuff
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var express = require('express');
 
 // Declare global variables
 var app = express();
 
 // Set and use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
 
+// Include controllers/routers
+app.use('/articles', require('./controllers/articles'));
+app.use('/authors', require('./controllers/authors'));
 
 // Define routes
 app.get('/', function(req, res){
-  res.send('home page!');
+  res.render('home');
 });
 
 // Hey! Listen!
