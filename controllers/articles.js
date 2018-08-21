3be587394858ab95b9a6571f934590564d8998b9
brandiw <brandimichelle.0911@gmail.com>
2018-08-21 09:37:49 -0700

    Added new forms for article and author

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.send('HELLO WORLD! - articles');
+  res.render('articles/index');
+});
+
+router.get('/new', function(req, res){
+  res.render('articles/new', {authors: []});
+});
+
+router.get('/:id', function(req, res){
+  res.send('article show page goes here');
+});
+
+router.post('/', function(req, res){
+  res.send('articles POST route reached');
 });
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.send('HELLO WORLD! - authors');
+  res.render('authors/index');
+});
+
+router.get('/new', function(req, res){
+  res.render('authors/new');
+});
+
+router.get('/:id', function(req, res){
+  res.send('author show page goes here');
+});
+
+router.post('/', function(req, res){
+  console.log(req.body);
+  res.send('/authors POST route reached');
 });
 
 module.exports = router;
